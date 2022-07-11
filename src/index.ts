a74ea2b88aebbc5ff3acea2b49e6f00a6d819496
ByeongJuP <pbju92@gmail.com>
2022-07-11 13:41:28 +0900

    add: graphQL playground plugins

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { ApolloServer } from 'apollo-server';
 import { schema } from './schema/schema';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
 export const server = new ApolloServer({
-  schema
+  schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground]
 });
 
 const port = 8000;
